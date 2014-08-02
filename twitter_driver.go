7c697ef78bcd661c7a86815e6595af2d1dcd16ee
yuhki50 <yuhki50@gmail.com>
2014-08-02 20:42:34 +0900

    adaptorを削除

--- twitter_driver.go
+++ twitter_driver.go
@@ -11,19 +11,19 @@ type TwitterDriver struct {
 type TwitterInterface interface {
 }
 
-func NewTwitterDriver(a *TwitterAdaptor, name string) *TwitterDriver {
+func NewTwitterDriver(name string) *TwitterDriver {
 	return &TwitterDriver{
 		Driver: *gobot.NewDriver(
 			name,
 			"twitter.TwitterDriver",
-			a,
 		),
 	}
 }
 
-func (t *TwitterDriver) adaptor() *TwitterAdaptor {
-	return t.Driver.Adaptor().(*TwitterAdaptor)
+func (t *TwitterDriver) Start() bool {
+	return true
 }
 
-func (t *TwitterDriver) Start() bool { return true }
-func (t *TwitterDriver) Halt() bool  { return true }
+func (t *TwitterDriver) Halt() bool {
+	return true
+}
